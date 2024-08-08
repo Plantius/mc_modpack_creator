329f240920bd20c9adaa11e21f96405784367ebb
niels <niels>
2024-08-08 13:31:29 +0200

    Initial Commit

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-
-r = requests.get('https://api.modrinth.com/v2/project/sodium')
-data = r.json()
-print(data['slug'])
--- src/main.cc
+++
@@ -1,7 +0,0 @@
-// Different project selector
-// Load project
-// Project consist of Name, Build Date, MC Version, Mod Loader, Pack Version, Mod List: Dependencies, Client only, Server only, mod updater
-// Compatability check
-// Delete project
-// Edit project
-// Create project
