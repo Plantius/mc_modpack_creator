e52153468cbac9fb48560e7dfa3350ec02cebe2c
niels <niels>
2024-08-19 01:43:49 +0200

    Small change to main.py

--- mc_modpack_creator/main.py
+++ mc_modpack_creator/main.py
@@ -22,7 +22,8 @@ def main():
     p = project.Project()
     # mod_list = []
     # for i in range(25):
-    #     mod_list.append(mod.Mod(f"{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}", 
+    #     mod_list.append(mod.Mod(f"{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}",
+    #                             f"{chr(random.randint(65, 121))*random.randint(1, 5)} {chr(random.randint(65, 121))*random.randint(1, 5)} {chr(random.randint(65, 121))*random.randint(1, 5)}", 
     #                             f"{random.randint(1, 20)}.{random.randint(1, 25)}", 
     #                             f"1.{random.randint(1, 21)}.{random.randint(0, 6)}", 
     #                             random.randint(0, 1), 
@@ -30,10 +31,10 @@ def main():
     #                             ["fabric", "forge", "quilt", "neoforge"][random.randint(0, 3)], 
     #                             f"{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}", 
     #                             "Plantius"))
-    # print(mod_list)
-    # p.create_project("First", build_version="0.1", mc_version="1.19", mod_loader="forge", mod_list=mod_list)
+        
+    # p.create_project("First", description="My first modpack.", build_version="0.1", mc_version="1.19", mod_loader="forge", mod_list=mod_list)
     # p.save_project("p1.json")
-    p.load_project("p1.json")
+    # p.load_project("p1.json")
     # Main Menu
     menu = main_menu.menu(p)
     menu.main_menu()
