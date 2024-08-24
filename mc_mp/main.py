8d7695c8e4e5c6c94a6e8339f5494e0281941501
niels <niels>
2024-08-25 00:11:30 +0200

    Added comments to main.py

--- mc_mp/main.py
+++ mc_mp/main.py
@@ -1,44 +1,21 @@
-# Different project selector
-# Load project
-# Project consist of Name, Build Date, MC Version, Mod Loader, Pack Version, Mod List: Dependencies, Client only, Server only, mod updater
-# Compatability check
-# Delete project
-# Edit project
-# Create project
-
-from modpack import project, mod
+from modpack import project
 from args_parser import args_parser as args
 from menu import main_menu
-import random
 
 def main():
-    flags = {"project_loaded": False, "project_saved": False, "project_valid": False, "compatibel": False}
+    # Initialize project and flags
+    p = project.Project()
 
-    # Parse arguments
+    # Parse command-line arguments
     arguments = args.parse_arguments()
     print(arguments)
 
-    # Project object
-    p = project.Project()
-    if arguments.open_project is not None:
+    # Load project if specified
+    if arguments.open_project:
         p.load_project(arguments.open_project)
-    # mod_list = []
-    # for i in range(25):
-    #     mod_list.append(mod.Mod(f"{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}",
-    #                             f"{chr(random.randint(65, 121))*random.randint(1, 5)} {chr(random.randint(65, 121))*random.randint(1, 5)} {chr(random.randint(65, 121))*random.randint(1, 5)}", 
-    #                             f"{random.randint(1, 20)}.{random.randint(1, 25)}", 
-    #                             f"1.{random.randint(1, 21)}.{random.randint(0, 6)}", 
-    #                             random.randint(0, 1), 
-    #                             random.randint(0, 1), 
-    #                             ["fabric", "forge", "quilt", "neoforge"][random.randint(0, 3)], 
-    #                             f"{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}{chr(random.randint(65, 121))}", 
-    #                             "Plantius"))
-    
-    # p.create_project(title="First", description="My first modpack.", build_version="0.1", mc_version="1.21", mod_loader="fabric", mod_list=mod_list)
-    # p.save_project("p1.json")
-    # p.load_project("p1.json")
-    # Main Menu
-    menu = main_menu.menu(p)
+
+    # Initialize and display main menu
+    menu = main_menu.Menu(p)
     menu.main_menu()
 
 if __name__ == "__main__":
